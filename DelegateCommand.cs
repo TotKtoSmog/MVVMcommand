17b49dea56248939fb75b49709ed4320d9e99d03
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-07-23 15:23:31 +0300

    Добавление класс DelegateCommand

---
+++ DelegateCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+
+namespace MVVMcommand
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+            => canExecute == null || canExecute(parameter);
+
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
