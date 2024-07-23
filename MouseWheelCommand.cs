bed391e3e6c3969e31dc33d1450f069ab0df3949
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-07-23 17:47:47 +0300

    Добавление класс MouseWheelCommand

---
+++ MouseWheelCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MVVMcommand
+{
+    public class MouseWheelCommand
+    {
+        public static readonly DependencyProperty MouseWheelCommandProperty =
+        DependencyProperty.RegisterAttached(
+            "MouseWheelCommand",
+            typeof(ICommand),
+            typeof(MouseWheelCommand),
+            new PropertyMetadata(null, OnMouseWheelCommandChanged));
+
+        public static void SetMouseWheelCommand(UIElement element, ICommand value)
+            => element.SetValue(MouseWheelCommandProperty, value);
+
+        public static ICommand GetMouseWheelCommand(UIElement element)
+            => (ICommand)element.GetValue(MouseWheelCommandProperty);
+
+        private static void OnMouseWheelCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                element.MouseWheel += (sender, args) =>
+                {
+                    var command = GetMouseWheelCommand(element);
+                    if (command != null && command.CanExecute(args))
+                    {
+                        command.Execute(args);
+                    }
+                };
+            }
+        }
+    }
+}
