1b7c624d99c2f2b3d77b1ca1d3d6e469e639af9b
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-07-23 17:01:18 +0300

    Добавление класс MouseButtonCommand

---
+++ MouseButtonCommand.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MVVMcommand
+{
+    public class MouseButtonCommand
+    {
+        public static readonly DependencyProperty MouseDownCommandProperty =
+        DependencyProperty.RegisterAttached(
+            "MouseDownCommand",
+            typeof(ICommand),
+            typeof(MouseButtonCommand),
+            new PropertyMetadata(null, OnMouseDownCommandChanged));
+
+        public static readonly DependencyProperty MouseUpCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "MouseUpCommand",
+                typeof(ICommand),
+                typeof(MouseButtonCommand),
+                new PropertyMetadata(null, OnMouseUpCommandChanged));
+
+        public static void SetMouseDownCommand(UIElement element, ICommand value)
+            => element.SetValue(MouseDownCommandProperty, value);
+
+        public static ICommand GetMouseDownCommand(UIElement element)
+            => (ICommand)element.GetValue(MouseDownCommandProperty);
+
+        public static void SetMouseUpCommand(UIElement element, ICommand value)
+            => element.SetValue(MouseUpCommandProperty, value);
+
+        public static ICommand GetMouseUpCommand(UIElement element)
+            => (ICommand)element.GetValue(MouseUpCommandProperty);
+
+        private static void OnMouseDownCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                element.MouseDown += (sender, args) =>
+                {
+                    var command = GetMouseDownCommand(element);
+                    if (command != null && command.CanExecute(args))
+                    {
+                        command.Execute(args);
+                    }
+                };
+            }
+        }
+
+        private static void OnMouseUpCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                element.MouseUp += (sender, args) =>
+                {
+                    var command = GetMouseUpCommand(element);
+                    if (command != null && command.CanExecute(args))
+                    {
+                        command.Execute(args);
+                    }
+                };
+            }
+        }
+    }
+}
